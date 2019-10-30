5010f7ee7cfeb3d73188e49c1b58ae31625bec97
Shenghao Yang <me@shenghaoyang.info>
2019-10-30 22:33:53 +0800

    Implement BFS and graph creation from a Reader object.

---
+++ src/main/java/Lab4/graph/Graph.java
@@ -0,0 +1,145 @@
+package Lab4.graph;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.*;
+import java.util.function.Consumer;
+
+/**
+ * Class representing a undirected and unweighted graph, with no self-edges
+ * and multiple edges between the same two nodes.
+ *
+ * Strings are used to uniquely identify each node.
+ */
+public class Graph {
+    private SortedMap<String, Node<String>> nodes;
+
+    /**
+     * Construct a new graph from a reader providing a graph in CSV format.
+     * @param r reader to load the graph from.
+     * @param sorter sorter function that determines the order of graph
+     *               traversal by determining the order of nodes within a
+     *               neighbor list of a node.
+     * @throws IOException on I/O error.
+     */
+    public Graph(Reader r,
+                 Consumer<ArrayList<Node<String>>> sorter) throws IOException {
+        /*
+         * Temporary storage to hold nodes as they are being read.
+         */
+        nodes = new TreeMap<>();
+
+        /*
+         * Build nodes into temporary storage.
+         */
+        try (var in = CSVParser.parse(r, CSVFormat.RFC4180)) {
+            for (var record : in) {
+                var rnum = in.getRecordNumber();
+
+                if (record.size() < 1)
+                    throw new IllegalArgumentException(String.format(
+                            "record %d: node unnamed / empty line.", rnum));
+
+                if (record.size() < 2)
+                    throw new IllegalArgumentException(String.format(
+                            "record %d: node has no neighbors.", rnum));
+
+                var nodeName = record.get(0);
+
+                for (var n : record)
+                    nodes.putIfAbsent(n, new Node<>(n));
+
+                Node<String> node = nodes.get(nodeName);
+                for (var i = 1; i < record.size(); ++i) {
+                    Node<String> toLink = nodes.get(record.get(i));
+
+                    if (node.isNeighbor(toLink))
+                        continue;
+
+                    node.addNeighbor(toLink);
+                }
+            }
+        }
+
+        /*
+         * Define the visitation order.
+         */
+        nodes.forEach((k, v) -> v.rearrangeNeighbors(sorter));
+    }
+
+    /**
+     * Rearrange the adjacency lists storing neighbor information in each
+     * of the nodes using the provided function.
+     *
+     * This affects the order in which nodes are visited during traversal.
+     *
+     * @param rearranger function to use to rearrange the adjacency lists.
+     */
+    public void rearrange(Consumer<ArrayList<Node<String>>> rearranger) {
+        nodes.forEach((k, v) -> v.rearrangeNeighbors(rearranger));
+    }
+
+
+    /**
+     * Performs a breath first search on the graph, attempting the locate
+     * a node, and recovering the path to that node.
+     *
+     * All collections passed to this method must have been cleared.
+     *
+     * The collections are not allocated internally in order to isolate
+     * the cost of creating these collections from the search time.
+     *
+     * Of course, the implementation backing these collection interfaces
+     * would change the performance of the search, but this is additional
+     * information the method does not know. Benchmarkers must take care to keep
+     * the implementations of these data structures unchanged to make
+     * sound performance comparisons.
+     *
+     * If the target node cannot be successfully located, there is no entry
+     * for that node in the map.
+     *
+     * @param s name of source node to start BFS from.
+     * @param t name of target node to find.
+     * @param pred map used to store node predecessor information.
+     * @param visited set used to mark nodes as visited.
+     * @param queue queue used to store nodes pending visitation.
+     * @throws IllegalArgumentException if the source or target node cannot
+     *                                  be found in the graph.
+     */
+    public void breadthFirstSearch(String s, String t,
+                                   Map<String, String> pred,
+                                   Set<String> visited,
+                                   Deque<Node<String>> queue) {
+
+        if ((!nodes.containsKey(s)) || (!nodes.containsKey(t)))
+            throw new IllegalArgumentException(
+                    "source / target node not contained in graph.");
+
+        queue.add(nodes.get(s));
+        while (!queue.isEmpty()) {
+            var n = queue.removeFirst();
+            visited.add(n.getName());
+
+            for (var neigh : n.getNeighbors()) {
+                if (visited.contains(neigh.getName()))
+                    continue;
+
+                queue.add(neigh);
+                pred.put(neigh.getName(), n.getName());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        var builder = new StringBuilder(String.format("Graph with nodes:%n"));
+
+        nodes.forEach((k, v) -> builder.append(String.format("%s%n",
+                v.toString())));
+
+        return builder.toString();
+    }
+}
---
+++ src/main/java/Lab4/graph/Node.java
@@ -0,0 +1,175 @@
+package Lab4.graph;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+/**
+ * Generic graph node for an undirected and unweighted graph, with a maximum of
+ * only one edge between any two nodes.
+ *
+ * Node that nodes of a particular type can only be linked to nodes
+ * of the same type.
+ *
+ * @param <K> type of the data used to name the node.
+ */
+public class Node<K extends Comparable<K>>
+        implements Comparable<Node<K>> {
+    /**
+     * Name of the node.
+     */
+    private K name;
+    /**
+     * Neighboring nodes connected to this node, in an ordered list
+     * to enforce visitation order.
+     */
+    private ArrayList<Node<K>> neighbors;
+    /**
+     * Set containing neighboring nodes, used for quick lookups.
+     */
+    private Set<Node<K>> neighborSet;
+
+    /**
+     * Construct a new node with an empty neighbor list.
+     *
+     * @param k name of the node.
+     */
+    public Node(K k) {
+        this(k, new LinkedList<>());
+    }
+
+    /**
+     * Construct a new node.
+     *
+     * @param k name of the node.
+     * @param n list of node neighbors. The order in which nodes are presented
+     *          in this list is the order of visitation any graph traversal
+     *          operation will use when traversing a graph containing this node.
+     */
+    public Node(K k, List<Node<K>> n) {
+        name = k;
+        this.neighbors = new ArrayList<>(n);
+        this.neighborSet = new HashSet<>(this.neighbors);
+    }
+
+    /**
+     * Yield a string representation of this node.
+     *
+     * The returned string will be in the form of:
+     *
+     * Node name: [Neighbor 0 node name, Neighbor 1 node name, ...]
+     *
+     * The neighbor names are written in visitation order.
+     *
+     * @return string representation of this node.
+     */
+    @Override
+    public String toString() {
+        var builder = new StringBuilder(
+                String.format("%s: [", getName()));
+
+        this.neighbors.forEach(n -> builder.append(
+                String.format("%s, ", n.getName())));
+
+        builder.append("]");
+
+        return builder.toString();
+    }
+
+    /**
+     * Compare this node with another node, using the name of the nodes
+     * to perform the comparison.
+     *
+     * This method relies on the natural order of the node names.
+     *
+     * @param n node to compare to.
+     * @return comparison result.
+     */
+    @Override
+    public int compareTo(Node<K> n) {
+        return name.compareTo(n.name);
+    }
+
+    /**
+     * Obtain the name of the node.
+     *
+     * @return node name.
+     */
+    public K getName() {
+        return name;
+    }
+
+    /**
+     * Check if this node is a neighbor of another node.
+     *
+     * @param n other node.
+     * @return neighbor check result.
+     */
+    public boolean isNeighbor(Node<K> n) {
+        return neighborSet.contains(n);
+    }
+
+    /**
+     * Add a node to the neighbor list.
+     *
+     * This node is placed at the end of the neighbor list, by default.
+     *
+     * If the other node does not have this node as its neighbor, then
+     * this node is added to the other node's neighbor list, following the
+     * above-mentioned semantics.
+     *
+     * @param n node to add to the neighbor list.
+     * @throws IllegalArgumentException when the node already exists
+     *                                  in the list.
+     */
+    public void addNeighbor(Node<K> n) {
+        if (neighborSet.contains(n))
+            throw new IllegalArgumentException(
+                    String.format("Node %s is already a neighbor of node %s.",
+                            n, this));
+
+        neighbors.add(n);
+        neighborSet.add(n);
+
+        if (!n.isNeighbor(this))
+            n.addNeighbor(this);
+    }
+
+    /**
+     * Update the neighbor list of this node to be the one specified.
+     *
+     * @param l new neighbor list to use.
+     */
+    public void updateNeighbors(List<Node<K>> l) {
+        neighbors.clear();
+        neighborSet.clear();
+        neighbors.addAll(l);
+        neighborSet.addAll(neighbors);
+    }
+
+    /**
+     * Obtain a view of the neighbor list of this node.
+     * Nodes are arranged in the order that they will be visited during graph
+     * traversal of a graph containing this node.
+     *
+     * After / during an operation to mutate the node's neighbor list,
+     * this view becomes / is invalid.
+     *
+     * @return list of neighbor nodes.
+     */
+    public List<Node<K>> getNeighbors() {
+        return Collections.unmodifiableList(neighbors);
+    }
+
+    /**
+     * Rearrange the neighbor list using a provided function.
+     *
+     * This allows changes to the neighbor visitation order.
+     *
+     * @param arranger sorter function that determines the order of graph
+     *                 traversal by determining the order of nodes within a
+     *                 neighbor list of a node.
+     */
+    public void rearrangeNeighbors(Consumer<ArrayList<Node<K>>> arranger) {
+        arranger.accept(neighbors);
+    }
+}
